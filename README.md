071921517237b5d21f37911ec73fc2eacaf20750
baosisi <18018021075@163.com>
2022-10-13 17:05:44 +0800

    add preview

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 安装
 
-`npm i @bss/wx-mini-cli -g`
+`npm i @baosisi07/wx-mini-cli -g`
 
 ## 命令行使用
 
@@ -8,7 +8,7 @@
 
 通过在项目下添加以下两个文件实现自定义配置：
 
-- 项目配置文件 [ci.config.js](https://github.com/baosisi07/taro-app/blob/main/ci.config.js)
+- 项目配置文件 [ci.config.js/ci.config.mjs](https://github.com/baosisi07/taro-app/blob/main/ci.config.js)
 - 项目版本等信息 [ci.version.json](https://github.com/baosisi07/taro-app/blob/main/ci.version.json)
 
 1. 创建页面或组件
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { Command } from 'commander'
 import createProgram from './lib/creatFile.js'
 import publishWeApp from './lib/publishWeApp.js'
 import openWeApp from './lib/openWeApp.js'
+import previewWeApp from './lib/previewWeApp.js'
 import Config from './config.js'
 import Utils from './utils/index.js'
 import Log from './log.js'
@@ -18,7 +19,8 @@ let userConf = null
 program.version(packageJson.version, '-v, --version');
 program.command('create').description('创建页面或组件').action((cmd, op) => createProgram(cmd))
 program.command('open').description('打开开发者工具').action(async (cmd, op) => openWeApp(await getUserConf()))
-program.command('publish').description('发布体验版').action(async(cmd, op) => publishWeApp(await getUserConf()) )
+program.command('publish').description('发布体验版').action(async (cmd, op) => publishWeApp(await getUserConf()))
+program.command('preview').description('预览体验版').action(async(cmd, op) => previewWeApp(await getUserConf()))
 // 自定义指令
 program
     .command('run <cmd>')
@@ -39,16 +41,18 @@ function setConfig(param) {
     Config.output = path.resolve(path.join(Config.dir_root, param.output || param.entry || ''));
 }
 async function getUserConf({level = 2} = {}) {
-    const userConfPath = `${Config.dir_root}/ci.config.js`
+    let userConfPath = `${Config.dir_root}/ci.config.js`
     if (!Utils.checkFileExist(userConfPath)) {
-        if (level == 2) {
-            Log.err('当前项目尚未创建ci.config.js文件');
-            return process.exit(1);
-        } else {
-            return null
+        userConfPath = `${Config.dir_root}/ci.config.mjs`
+        if (!Utils.checkFileExist(userConfPath)) {
+            if (level == 2) {
+                Log.err('当前项目尚未创建ci.config.js文件');
+                return process.exit(1);
+            } else {
+                return null
+            }
         }
     }
-
     let data = userConf = (await import(userConfPath)).default
     if (!!data.template) {
         Config.template = path.resolve(path.join(Config.dir_root, data.template))
