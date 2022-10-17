50cf4b4a9550340606e6e746512baf4e064abd60
baosisi <18018021075@163.com>
2022-10-17 14:31:49 +0800

    replace command with minimist

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env babel-node
 import fs from 'fs'
-
-import { Command } from 'commander'
+import minimist from 'minimist'
 import createProgram from './lib/creatFile.js'
 import publishWeApp from './lib/publishWeApp.js'
 import openWeApp from './lib/openWeApp.js'
@@ -11,29 +10,44 @@ import Utils from './utils/index.js'
 import Log from './log.js'
 import path from 'path'
 import chalk from 'chalk'
-const program = new Command();
 const packagePath = path.resolve(path.join(Config.root, './package.json'))
 const packageJson = JSON.parse(fs.readFileSync(packagePath))
 
 let userConf = null
 
-// 设置版本号
-program.version(packageJson.version, '-v, --version');
-program.command('create').description('创建页面或组件').action((cmd, op) => createProgram(cmd))
-program.command('open').description('打开开发者工具').action(async (cmd, op) => openWeApp(await getUserConf()))
-program.command('publish').description('发布体验版').action(async (cmd, op) => publishWeApp(await getUserConf()))
-program.command('preview').description('预览体验版').action(async(cmd, op) => previewWeApp(await getUserConf()))
-// 自定义指令
-program
-    .command('run <cmd>')
-    .description('当前<cmd>包含: ' + getCustomScriptsDesc(await getUserConf({level: 1})))
-    .action(async (cmd, options) => {
-        // 当前命令
-        let curScript = (await getUserConf()).customScripts.find((el, idx) => el.name === cmd);
+const args = minimist(process.argv.slice(2), {
+    alias: {
+      version: ['v'],
+      help: ['h'],
+    },
+    boolean: ['version', 'help']
+})
+const _ = args._
 
-        // 执行回调
-        await curScript.callback.call(originPrototype, cmd, options);
-    });
+if (args.version) {
+    console.log(chalk.green(packageJson.version))
+}
+if (args.help) {
+    console.log(chalk.green('mini-wechat-cli create 创建页面或组件模版'))
+    console.log(chalk.green('mini-wechat-cli open 在微信开发工具打开项目'))
+    console.log(chalk.green('mini-wechat-cli preview 生成预览二维码 会显示在小程序助手'))
+    console.log(chalk.green('mini-wechat-cli publish 上传代码并发布版本'))
+}
+const command = _[0]
+switch (command) {
+    case 'create':
+        createProgram(await getUserConf())
+        break;
+    case 'open':
+        openWeApp(await getUserConf())
+    break;
+    case 'preview':
+        previewWeApp(await getUserConf())
+    break;
+    case 'publish':
+        publishWeApp(await getUserConf())
+        break;
+}
 
 function setConfig(param) {
     // 小程序入口目录
@@ -63,11 +77,4 @@ async function getUserConf({level = 2} = {}) {
     setConfig(data)
     return data
 }
-// 获取自定义指令描述信息     
-function getCustomScriptsDesc(param) {
-    return (param?.customScripts?.length) 
-                ? (param.customScripts.map(el => chalk.yellow(el.name) + ' -> ' + chalk.blue(el.desc)).join(' | '))
-                : '当前项目尚无自定义命令';
-}
-// 输出文本
-program.parse(process.argv)
+
